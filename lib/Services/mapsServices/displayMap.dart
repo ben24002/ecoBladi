2d09d289318b8b685f523727c6ff50d5fddf5782
hichemKoudja <hichem20202001@gmail.com>
2024-04-18 13:58:24 +0100

    Commit message

--- lib/Services/mapsServices/displayMap.dart
+++ lib/Services/mapsServices/displayMap.dart
@@ -1,19 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart'; // Import Google Maps Flutter package
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class DisplayMap extends StatelessWidget {
-  const DisplayMap({super.key});
+class DisplayMap extends StatefulWidget {
+  // ignore: use_super_parameters
+  const DisplayMap({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _DisplayMapState createState() => _DisplayMapState();
+}
+
+class _DisplayMapState extends State<DisplayMap> {
+  late GoogleMapController _mapController;
 
   @override
   Widget build(BuildContext context) {
     return GoogleMap(
-      initialCameraPosition: const CameraPosition(
-        target: LatLng(37.7749, -122.4194), // Set initial position to San Francisco
-        zoom: 12, // Adjust the zoom level as needed
+      initialCameraPosition: CameraPosition(
+        target: LatLng(36.7372, 3.0867), // Algiers coordinates
+        zoom: 12,
       ),
-      onMapCreated: (GoogleMapController controller) {
-        // You can use the controller here if needed
-      },
+      onMapCreated: _onMapCreated,
+      markers: _createMarkers(),
     );
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      _mapController = controller;
+    });
+  }
+
+  Set<Marker> _createMarkers() {
+    return <Marker>[
+      Marker(
+        markerId: MarkerId('algiers_marker'),
+        position: LatLng(36.7372, 3.0867), // Algiers coordinates
+        infoWindow: InfoWindow(
+          title: 'Algiers',
+          snippet: 'Capital of Algeria',
+        ),
+      ),
+    ].toSet();
+  }
+
+  @override
+  void dispose() {
+    _mapController.dispose();
+    super.dispose();
   }
 }
